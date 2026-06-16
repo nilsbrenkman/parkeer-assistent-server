10b83c85c25e0fff2fb51f29ffa991e146c6d188
Nils Brenkman <nils@brenkman.eu>
2026-06-16 17:11:09 +0200

    Add endpoint for fetching parking meter details by ID with corresponding tests

--- src/main/kotlin/nl/parkeerassistent/route/GeoRoutes.kt
+++ src/main/kotlin/nl/parkeerassistent/route/GeoRoutes.kt
@@ -1,6 +1,8 @@
 package nl.parkeerassistent.route
 
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.plugins.BadRequestException
+import io.ktor.server.plugins.NotFoundException
 import io.ktor.server.response.respond
 import io.ktor.server.routing.get
 import io.ktor.server.routing.route
@@ -27,6 +29,14 @@ val geoRoutes: RouterGroup = {
             val lon = call.request.queryParameters["lon"]?.toDoubleOrNull() ?: throw BadRequestException("lon")
             Metrics.logAndCount(call, GeoService.Method.ParkingMeters, Level.INFO, "REGION")
             call.respond(GeoService.getParkingMetersInRegion(Point.from(lat, lon), Point.from(lat, lon)))
+        }
+
+        get("/parking-meters/{id}") {
+            try {
+                call.respond(GeoService.details(call))
+            } catch (_: NotFoundException) {
+                call.respond(HttpStatusCode.NotFound)
+            }
         }
     }
 
--- src/test/kotlin/nl/parkeerassistent/route/GeoRoutesKtTest.kt
+++ src/test/kotlin/nl/parkeerassistent/route/GeoRoutesKtTest.kt
@@ -31,4 +31,30 @@ class GeoRoutesKtTest {
         assertEquals("ZACHARIAS JANSESTRAAT 36 T/H", parkingMeters[2].name)
     }
 
+    @Test
+    fun `test fetching a parking meter by its id`() = testApplication {
+        application {
+            module()
+        }
+        val response = client.get("/geo/parking-meters/15505") {
+            cookie("token", "XXX")
+        }
+        assert(response.status.value == 200)
+        val body = response.bodyAsText()
+        val parkingMeter = Json.decodeFromString<ParkingMeter>(body)
+        assertEquals(15505, parkingMeter.id)
+    }
+
+    @Test
+    fun `test fetching a parking meter that doesn't exist`() = testApplication {
+        application {
+            module()
+        }
+        val response = client.get("/geo/parking-meters/99999") {
+            cookie("token", "XXX")
+        }
+        assert(response.status.value == 404)
+    }
+
+
 }
